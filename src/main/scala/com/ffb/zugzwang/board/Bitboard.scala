e4b7fcf83d70d90728182b461cb4a0141154ac32
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-04-19 09:17:12 -0400

    Add Bitboard implementation and tests

---
+++ src/main/scala/com/ffb/zugzwang/board/Bitboard.scala
@@ -0,0 +1,94 @@
+package com.ffb.zugzwang.board
+
+import com.ffb.zugzwang.chess.{Rank, Square}
+
+opaque type Bitboard = Long
+
+object Bitboard:
+  val empty: Bitboard = 0L
+  val full: Bitboard = 0xffffffffffffffffL
+
+  val diagonal: Bitboard = 0x0102040810204080L
+  val antiDiagonal: Bitboard = 0x8040201008040201L
+
+  val fileA: Bitboard = 0x8080808080808080L
+  val fileB: Bitboard = fileA >> 1
+  val fileC: Bitboard = fileB >> 1
+  val fileD: Bitboard = fileC >> 1
+  val fileE: Bitboard = fileD >> 1
+  val fileF: Bitboard = fileE >> 1
+  val fileG: Bitboard = fileF >> 1
+  val fileH: Bitboard = fileH >> 1
+
+  val rank1: Bitboard = 0xffL
+  val rank2: Bitboard = rank1 << 8
+  val rank3: Bitboard = rank2 << 8
+  val rank4: Bitboard = rank3 << 8
+  val rank5: Bitboard = rank4 << 8
+  val rank6: Bitboard = rank5 << 8
+  val rank7: Bitboard = rank6 << 8
+  val rank8: Bitboard = rank7 << 8
+
+  val f1g1Mask: Bitboard = 6L
+  val b1c1d1Mask: Bitboard = 0x70L
+  val f8g8mask: Bitboard = 0x600000000000000L
+  val b8c8d8Mask: Bitboard = 0x7000000000000000L
+
+  def from(sq: Square): Bitboard = 1L << sq.value
+
+  def from(sqs: Square*): Bitboard =
+    sqs.foldLeft(0L: Bitboard)((bb, sq) => bb | (1 << sq.value))
+
+  def fileMaskFor(sq: Square): Bitboard = sq.file.value match {
+    case 0 => fileH
+    case 1 => fileG
+    case 2 => fileF
+    case 3 => fileE
+    case 4 => fileD
+    case 5 => fileC
+    case 6 => fileB
+    case 7 => fileA
+  }
+
+  def ranKMaskFor(sq: Square): Bitboard = sq.rank.value match {
+    case 0 => rank1
+    case 1 => rank2
+    case 2 => rank3
+    case 3 => rank4
+    case 4 => rank5
+    case 5 => rank6
+    case 6 => rank7
+    case 7 => rank8
+  }
+
+  def reverse(bb: Bitboard): Bitboard = java.lang.Long.reverse(bb.value)
+
+  extension (bb: Bitboard)
+
+    inline def value: Long = bb
+    inline def toLong: Long = bb
+
+    inline def unary_~ : Bitboard = ~bb
+    inline def &(other: Bitboard): Bitboard = bb & other
+    inline def |(other: Bitboard): Bitboard = bb | other
+    inline def ^(other: Bitboard): Bitboard = bb ^ other
+    inline def <<(other: Bitboard): Bitboard = bb << other
+    inline def >>>(other: Bitboard): Bitboard = bb >>> other
+
+    inline def isEmpty: Boolean = bb == empty
+    inline def nonEmpty: Boolean = bb != empty
+
+    inline def setBitAt(sq: Square): Bitboard = bb | (1L << sq.value)
+
+    inline def clearBitAt(sq: Square): Bitboard = bb & ~(1L << sq.value)
+
+    def leastSignificantBit: Option[Square] =
+      Square.from(java.lang.Long.numberOfTrailingZeros(bb))
+
+    def removeLsb: Bitboard = bb & (bb - 1L)
+
+    def popCount: Bitboard = java.lang.Long.bitCount(bb)
+
+  end extension
+
+end Bitboard
---
+++ src/test/scala/com/ffb/zugzwang/board/BitboardSpec.scala
@@ -0,0 +1,70 @@
+package com.ffb.zugzwang.board
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import com.ffb.zugzwang.chess.Square
+
+class BitboardSpec extends AnyFlatSpec with Matchers:
+
+  "Bitboard.fromSquare" should "produce a board with exactly one bit set" in {
+    Square.from(0) map { sq =>
+      val bb = Bitboard.from(sq)
+      bb.toLong shouldEqual 1L
+    }
+
+    Square.from(63) map { sq =>
+      val bb = Bitboard.from(sq)
+      bb.toLong shouldEqual (1L << 63)
+    }
+  }
+
+  it should "be empty when constructed from no squares" in {
+    Bitboard.empty.isEmpty shouldBe true
+    Bitboard.empty.popCount shouldEqual 0
+  }
+
+  "Bitboard.add and remove" should "toggle individual bits correctly" in {
+    val squares = List(Square.from(0), Square.from(7), Square.from(63)).flatten
+
+    squares foreach { sq =>
+      val b = Bitboard.empty
+      val setBit = b.setBitAt(sq)
+
+      setBit.isEmpty shouldBe false
+      setBit.popCount shouldEqual 1
+      setBit.leastSignificantBit shouldBe Some(sq)
+
+      val clearedBit = setBit.clearBitAt(sq)
+
+      clearedBit.isEmpty shouldBe true
+      clearedBit.popCount shouldEqual 0
+      clearedBit.leastSignificantBit shouldBe None
+    }
+  }
+
+  "Bitboard.|" should "combine two bitboards" in {
+    Square.from(0) flatMap { sq1 =>
+      Square.from(1) map { sq2 =>
+        {
+          val or = Bitboard.from(sq1) | Bitboard.from(sq2)
+          or.popCount shouldEqual 2
+          or.value shouldEqual 3L
+        }
+      }
+    }
+  }
+
+  "Bitboard.clearLsb" should "remove only the least significant bit" in {
+    Square.from(0) flatMap { sq1 =>
+      Square.from(1) map { sq2 =>
+        {
+          val bb = Bitboard.from(sq1, sq2)
+
+          bb.leastSignificantBit shouldEqual Some(0)
+          val cleared = bb.removeLsb
+
+          cleared.value shouldEqual 2L
+        }
+      }
+    }
+  }
