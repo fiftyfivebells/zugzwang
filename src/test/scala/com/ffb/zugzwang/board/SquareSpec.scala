939c1f4784a2655f33b54c683d9dfa27fc6d72ae
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-04-21 20:50:39 -0400

    Switch tests to AnyFlatSpec with Matchers

--- src/test/scala/com/ffb/zugzwang/board/SquareSpec.scala
+++ src/test/scala/com/ffb/zugzwang/board/SquareSpec.scala
@@ -1,70 +1,70 @@
 package com.ffb.zugzwang.board
 
-import org.scalatest.funsuite.AnyFunSuite
-import com.ffb.zugzwang.chess.Square
-import com.ffb.zugzwang.chess.File
+import com.ffb.zugzwang.chess.{File, Square}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
-class SquareSpec extends AnyFunSuite:
+class SquareSpec extends AnyFlatSpec with Matchers:
 
-  test(
-    "fromAlgebraic should convert 'h1' to a valid Square which toAlgebraic returns 'h1'"
-  ):
+  "Square.fromAlgebraic" should "convert 'h1' to a valid Square which toAlgebraic converts back to 'h1'" in {
     val result = Square.fromAlgebraic("h1")
-    assert(result.isRight, "Expected Right for input 'h1'")
+
+    result.isRight shouldBe true
     result match
       case Right(square) =>
         val alg = Square.toAlgebraic(square)
-        assert(alg == "h1", s"toAlgebraic returned '$alg', expected 'h1'")
+        alg shouldBe "h1"
 
       case Left(error) =>
         fail(s"Unexpected error for 'h1': $error")
+  }
 
-  test("toAlgebraic should convert a Square created with from(7, 0) to 'h1'"):
-    val squareResult = Square.from(7, 0)
-    assert(squareResult.isRight, "Square.from(0, 0) should succeed")
-    squareResult match
-      case Right(square) =>
-        val alg = Square.toAlgebraic(square)
-        assert(alg == "h1", s"Expected 'h1' but got '$alg'")
-
-      case Left(err) =>
-        fail(s"Unexpected error in smart constructor: $err")
-
-  test(
-    "fromAlgebraic should convert 'a8' to a valid Square which toAlgebraic returns 'a8'"
-  ):
+  "Square.fromAlgebraic" should "convert 'a8' to a valid Square with toAlgebraic converts back to 'a8'" in {
     val result = Square.fromAlgebraic("a8")
-    assert(result.isRight, "Expected Right for input 'a8'")
+
+    result.isRight shouldBe true
     result match
       case Right(square) =>
         val alg = Square.toAlgebraic(square)
-        assert(alg == "a8", s"Expected 'a8' but got '$alg'")
+        alg shouldBe "a8"
 
       case Left(error) =>
         fail(s"Unexpected error for 'a8': $error")
+  }
 
-  test("fromAlgebraic should return Left for invalid inputs"):
+  "Square.toAlgebraic" should "convert a Square created with from(7, 0) to 'h1'" in {
+    val squareResult = Square.from(7, 0)
+
+    squareResult.isRight shouldBe true
+    squareResult match
+      case Right(square) =>
+        val alg = Square.toAlgebraic(square)
+        alg shouldBe "h1"
+
+      case Left(err) =>
+        fail(s"Unexpected error in smart constructor: $err")
+  }
+
+  "Square.fromAlgebraic" should "return Left for invalid inputs" in {
     val inputs = List("z9", "a0", "11", "abc", "")
     inputs.foreach { input =>
       val result = Square.fromAlgebraic(input)
-      assert(result.isLeft, s"Expected Left for input '$input'")
+      result.isLeft shouldBe true
     }
+  }
 
-  test(
-    "Smart constructor from(file, rank) should return Left for invalid coordinates"
-  ):
+  "Square.from(file, rank)" should "return Left for invalid coordinates" in {
     assert(Square.from(-1, 0).isLeft, "Expected failure for file = -1")
     assert(Square.from(0, 8).isLeft, "Expected failure for rank = 8")
     assert(
       Square.from(8, 8).isLeft,
       "Expected failure for file and rank out of range"
     )
+  }
 
-  test(
-    "Smart constructor from(file, rank) should succeed for valid coordinates"
-  ):
+  "Square.from(file, rank)" should "succeed for valid coordinates" in {
     val result = Square.from(4, 3)
-    assert(result.isRight, "Expected success for coordinates (0, 0)")
+    result.isRight shouldBe true
     result match
       case Right(square) =>
         val alg = Square.toAlgebraic(square)
@@ -72,3 +72,4 @@ class SquareSpec extends AnyFunSuite:
 
       case Left(err) =>
         fail(s"Unexpected error for valid coordinates: $err")
+  }
