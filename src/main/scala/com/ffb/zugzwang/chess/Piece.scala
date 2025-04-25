508c0572608e9bce0efc612760194051e2202466
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-04-25 07:45:05 -0400

    Add helper function and fix some typos

--- src/main/scala/com/ffb/zugzwang/chess/Piece.scala
+++ src/main/scala/com/ffb/zugzwang/chess/Piece.scala
@@ -10,9 +10,26 @@ enum PieceType(val name: String):
 
 end PieceType
 
-case class Piece(pieceType: PieceType, color: Color):
+case class Piece(color: Color, pieceType: PieceType):
   override def toString: String = color match
-    case Color.White => pieceType.name
-    case Color.Black => pieceType.name.toUpperCase
+    case Color.White => pieceType.name.toUpperCase
+    case Color.Black => pieceType.name
+
+end Piece
+
+object Piece:
+
+  def from(c: Char): Piece =
+    val color = if c.isUpper then Color.White else Color.Black
+    val pieceType = c.toLower match {
+      case 'p' => PieceType.Pawn
+      case 'n' => PieceType.Knight
+      case 'b' => PieceType.Bishop
+      case 'r' => PieceType.Rook
+      case 'q' => PieceType.Queen
+      case 'k' => PieceType.King
+    }
+
+    Piece(color, pieceType)
 
 end Piece
