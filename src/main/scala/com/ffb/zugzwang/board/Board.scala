8b51cee7d4ace16eab6b9e8f64c301e1f9b0feaf
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-04-25 07:44:14 -0400

    Update the Board trait

--- src/main/scala/com/ffb/zugzwang/board/Board.scala
+++ src/main/scala/com/ffb/zugzwang/board/Board.scala
@@ -2,11 +2,10 @@ package com.ffb.zugzwang.board
 
 import com.ffb.zugzwang.chess.{Color, Piece, Square}
 
-sealed trait Board extends Product with Serializable:
-  def fromFen(fen: String): Board
+trait Board extends Product with Serializable:
   def toFen: String
   def clearBoard: Board
-  def pieceAt(sq: Square): Piece
+  def pieceAt(sq: Square): Option[Piece]
   def putPieceAt(p: Piece, sq: Square): Board
   def removePieceFrom(sq: Square): Board
   def isKingAttacked(c: Color): Boolean
