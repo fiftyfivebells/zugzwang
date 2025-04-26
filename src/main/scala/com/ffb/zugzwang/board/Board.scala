70addd85850a28c95ee670f3f66be5dd717ff3a3
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-04-26 07:30:51 -0400

    Make code focused on only one main implementation

--- src/main/scala/com/ffb/zugzwang/board/Board.scala
+++ src/main/scala/com/ffb/zugzwang/board/Board.scala
@@ -1,15 +1,146 @@
 package com.ffb.zugzwang.board
 
+import scala.collection.immutable.ArraySeq
 import com.ffb.zugzwang.chess.{Color, Piece, Square}
+import com.ffb.zugzwang.chess.PieceType
+import scala.collection.mutable
 
-trait Board extends Product with Serializable:
-  def toFen: String
-  def clearBoard: Board
-  def pieceAt(sq: Square): Option[Piece]
-  def putPieceAt(p: Piece, sq: Square): Board
-  def removePieceFrom(sq: Square): Board
-  def isKingAttacked(c: Color): Boolean
-  def isAttacked(sq: Square, c: Color): Boolean
-  def isAttackedByPawn(sq: Square, c: Color): Boolean
+enum PieceCategory:
+  case WP, WN, WB, WR, WQ, WK, BP, BN, BB, BR, BQ, BK
+
+object PieceCategory:
+  def of(piece: Piece): PieceCategory = piece match {
+    case Piece(Color.White, PieceType.Pawn)   => WP
+    case Piece(Color.White, PieceType.Knight) => WN
+    case Piece(Color.White, PieceType.Bishop) => WB
+    case Piece(Color.White, PieceType.Rook)   => WR
+    case Piece(Color.White, PieceType.Queen)  => WQ
+    case Piece(Color.White, PieceType.King)   => WK
+    case Piece(Color.Black, PieceType.Pawn)   => BP
+    case Piece(Color.Black, PieceType.Knight) => BN
+    case Piece(Color.Black, PieceType.Bishop) => BB
+    case Piece(Color.Black, PieceType.Rook)   => BR
+    case Piece(Color.Black, PieceType.Queen)  => BQ
+    case Piece(Color.Black, PieceType.King)   => BK
+  }
+end PieceCategory
+
+final case class Board private (
+    pieces: IArray[Bitboard],
+    squares: ArraySeq[Option[Piece]]
+):
+
+  // TODO: this is not the most "functional" implementation, so maybe I'll revisit this
+  // and try to do this in a nicer way later on. it works for now though
+  def toFen: String =
+    val fenString = new mutable.StringBuilder("")
+
+    for (rank <- 8 to 1 by -1) {
+      var emptySquares = 0
+      var startingSquare = rank * 8 - 1
+
+      for (i <- startingSquare to startingSquare - 7 by -1) {
+        squares(i) match {
+          case None => emptySquares += 1
+          case Some(p) =>
+            if emptySquares > 0 then
+              fenString.append(emptySquares.toString)
+              emptySquares = 0
+
+            fenString.append(p.toString)
+        }
+      }
+
+      if emptySquares > 0 then fenString.append(emptySquares.toString)
+
+      fenString.append("/")
+    }
+
+    fenString.dropRight(1).toString
+
+  def clearBoard: Board = Board.empty
+
+  def pieceAt(sq: Square): Option[Piece] = squares(sq.value)
+
+  def putPieceAt(p: Piece, sq: Square): Board =
+    val pieceIndex = PieceCategory.of(p).ordinal
+    val newBitboard = pieces(pieceIndex).setBitAt(sq)
+
+    Board(
+      pieces.updated(pieceIndex, newBitboard),
+      squares.updated(sq.value, Some(p))
+    )
+
+  def removePieceFrom(sq: Square): Board =
+    squares(sq.value) match {
+      case None => this
+      case Some(piece) =>
+        val pieceIndex = PieceCategory.of(piece).ordinal
+        val newBitboard = pieces(pieceIndex).clearBitAt(sq)
+        val newSquares = squares.updated(sq.value, None)
+
+        Board(pieces.updated(pieceIndex, newBitboard), newSquares)
+    }
+
+  def isKingAttacked(c: Color): Boolean =
+    val kingIndex =
+      if c == Color.White then PieceCategory.WK else PieceCategory.BK
+
+    val kingSquare = pieces(kingIndex.ordinal).leastSignificantBit
+
+    kingSquare match
+      case Some(square) => isAttacked(square, c)
+
+      // if we can't find the king, then there's a bigger problem, but since that's unlikely
+      // I'm just going to mark it as false
+      case None => false
+
+  def isAttacked(sq: Square, c: Color): Boolean = ???
+
+  def isAttackedByPawn(sq: Square, c: Color): Boolean = ???
+
+  private def pieceToCategory(p: Piece): PieceCategory = p match
+    case Piece(Color.White, PieceType.Pawn)   => PieceCategory.WP
+    case Piece(Color.White, PieceType.Knight) => PieceCategory.WN
+    case Piece(Color.White, PieceType.Bishop) => PieceCategory.WB
+    case Piece(Color.White, PieceType.Rook)   => PieceCategory.WR
+    case Piece(Color.White, PieceType.Queen)  => PieceCategory.WQ
+    case Piece(Color.White, PieceType.King)   => PieceCategory.WK
+    case Piece(Color.Black, PieceType.Pawn)   => PieceCategory.BP
+    case Piece(Color.Black, PieceType.Knight) => PieceCategory.BN
+    case Piece(Color.Black, PieceType.Bishop) => PieceCategory.BB
+    case Piece(Color.Black, PieceType.Rook)   => PieceCategory.BR
+    case Piece(Color.Black, PieceType.Queen)  => PieceCategory.BQ
+    case Piece(Color.Black, PieceType.King)   => PieceCategory.BK
+
+end Board
+
+object Board:
+
+  def empty: Board =
+    Board(IArray.fill(12)(Bitboard.empty), ArraySeq.fill(64)(None))
+
+  def initial: Board =
+    Board.from("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
+
+  def from(fen: String): Board =
+    val fenNoDashes = fen.replaceAll("/", "")
+    val fenNoNumbers =
+      fenNoDashes
+        .map(ch => if ch.isDigit then "*" * ch.asDigit else ch.toString)
+        .mkString
+
+    fenNoNumbers.zipWithIndex.foldLeft(Board.empty) { (bb, pair) =>
+      val (c, i) = pair
+      val index = 63 - i
+      if c == '*' then bb
+      else
+        val piece = Piece.from(c)
+
+        Square.from(index) match {
+          case Some(square) => bb.putPieceAt(piece, square)
+          case None         => bb
+        }
+    }
 
 end Board
--- src/test/scala/com/ffb/zugzwang/board/BoardSpec.scala
+++ src/test/scala/com/ffb/zugzwang/board/BoardSpec.scala
@@ -4,10 +4,10 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import com.ffb.zugzwang.chess.{Color, Piece, PieceType, Square}
 
-class BitboardBoardSpec extends AnyFlatSpec with Matchers:
+class BoardSpec extends AnyFlatSpec with Matchers:
 
   ".toFen" should "convert a board into a string representing the fen of the position" in {
-    val initial = BitboardBoard.initial
+    val initial = Board.initial
 
     val fen = initial.toFen
     val initialBoardFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
@@ -15,42 +15,42 @@ class BitboardBoardSpec extends AnyFlatSpec with Matchers:
     fen shouldBe initialBoardFen
   }
 
-  "BitboardBoard.from(fen)" should "correctly add pieces to board based on fen string" in {
+  "Board.from(fen)" should "correctly add pieces to board based on fen string" in {
     val initialBoardFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
 
-    val bbBoard = BitboardBoard.from(initialBoardFen)
+    val board = Board.from(initialBoardFen)
     val a1 = Square.from(7, 0).right.get
     val e1 = Square.from(4, 0).right.get
     val e4 = Square.from(4, 3).right.get
 
-    bbBoard.pieceAt(e1) shouldBe Some(Piece(Color.White, PieceType.King))
-    bbBoard.pieceAt(a1) shouldBe Some(Piece(Color.White, PieceType.Rook))
-    bbBoard.pieceAt(e4) shouldBe None
+    board.pieceAt(e1) shouldBe Some(Piece(Color.White, PieceType.King))
+    board.pieceAt(a1) shouldBe Some(Piece(Color.White, PieceType.Rook))
+    board.pieceAt(e4) shouldBe None
   }
 
   ".putPieceAt(square)" should "add a piece at the given square" in {
-    val bbBoard = BitboardBoard.empty
+    val board = Board.empty
 
     Square.fromAlgebraic("a1") map { sq =>
       val pawn = Piece(Color.White, PieceType.Pawn)
-      val updated = bbBoard.putPieceAt(pawn, sq)
-      updated == BitboardBoard.empty shouldBe false
+      val updated = board.putPieceAt(pawn, sq)
+      updated == Board.empty shouldBe false
       updated.pieceAt(sq) shouldBe Some(pawn)
     }
 
     Square.fromAlgebraic("e4") map { sq =>
       val king = Piece(Color.Black, PieceType.King)
-      val updated = bbBoard.putPieceAt(king, sq)
-      updated == BitboardBoard.empty shouldBe false
+      val updated = board.putPieceAt(king, sq)
+      updated == Board.empty shouldBe false
       updated.pieceAt(sq) shouldBe Some(king)
     }
   }
 
   ".removePieceFrom(square)" should "remove the piece from the given square" in {
-    val bbBoard = BitboardBoard.initial
+    val board = Board.initial
 
     Square.fromAlgebraic("e1") map { sq =>
-      val updated = bbBoard.removePieceFrom(sq)
+      val updated = board.removePieceFrom(sq)
       updated.pieceAt(sq) shouldBe None
     }
   }
