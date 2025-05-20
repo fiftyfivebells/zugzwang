158a52fe7ff74487f750d50c9d0abad52c799c14
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-05-19 22:03:54 -0400

    Add an implicit ordering to PieceType and formatting changes

--- src/main/scala/com/ffb/zugzwang/chess/Piece.scala
+++ src/main/scala/com/ffb/zugzwang/chess/Piece.scala
@@ -12,6 +12,19 @@ enum PieceType(val name: String):
 
 end PieceType
 
+object PieceType:
+  given Ordering[PieceType] with
+    def compare(x: PieceType, y: PieceType): Int = value(y).compare(value(x))
+
+  private def value(pt: PieceType): Int = pt match {
+    case Pawn   => 10
+    case Knight => 30
+    case Bishop => 35
+    case Rook   => 50
+    case Queen  => 90
+    case King   => 1000
+  }
+
 case class Piece(color: Color, pieceType: PieceType):
   override def toString: String = color match
     case Color.White => pieceType.name.toUpperCase
@@ -37,16 +50,16 @@ object Piece:
   def from(pc: PieceCategory): Piece = pc match {
     case PieceCategory.WP => Piece(Color.White, PieceType.Pawn)
     case PieceCategory.WN => Piece(Color.White, PieceType.Knight)
-    case PieceCategory.WB               => Piece(Color.White, PieceType.Bishop)
-    case PieceCategory.WR               => Piece(Color.White, PieceType.Rook)
-    case PieceCategory.WQ               => Piece(Color.White, PieceType.Queen)
-    case PieceCategory.WK               => Piece(Color.White, PieceType.King)
-    case PieceCategory.BP               => Piece(Color.Black, PieceType.Pawn)
-    case PieceCategory.BN               => Piece(Color.Black, PieceType.Knight)
-    case PieceCategory.BB               => Piece(Color.Black, PieceType.Bishop)
-    case PieceCategory.BR               => Piece(Color.Black, PieceType.Rook)
-    case PieceCategory.BQ               => Piece(Color.Black, PieceType.Queen)
-    case PieceCategory.BK               => Piece(Color.Black, PieceType.King)
+    case PieceCategory.WB => Piece(Color.White, PieceType.Bishop)
+    case PieceCategory.WR => Piece(Color.White, PieceType.Rook)
+    case PieceCategory.WQ => Piece(Color.White, PieceType.Queen)
+    case PieceCategory.WK => Piece(Color.White, PieceType.King)
+    case PieceCategory.BP => Piece(Color.Black, PieceType.Pawn)
+    case PieceCategory.BN => Piece(Color.Black, PieceType.Knight)
+    case PieceCategory.BB => Piece(Color.Black, PieceType.Bishop)
+    case PieceCategory.BR => Piece(Color.Black, PieceType.Rook)
+    case PieceCategory.BQ => Piece(Color.Black, PieceType.Queen)
+    case PieceCategory.BK => Piece(Color.Black, PieceType.King)
   }
 
 end Piece
