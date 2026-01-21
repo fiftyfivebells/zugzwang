6d9fd18c76270a0944d18cd7a9c334e7dc8c0e32
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2026-01-21 18:47:40 -0500

    Add mutable versions of move gen functions

--- src/main/scala/com/ffb/zugzwang/move/MoveGenerator.scala
+++ src/main/scala/com/ffb/zugzwang/move/MoveGenerator.scala
@@ -1,7 +1,7 @@
 package com.ffb.zugzwang.move
 
 import com.ffb.zugzwang.board.{Bitboard, Board}
-import com.ffb.zugzwang.chess.{CastleRights, CastleSide, Color, GameState, Piece, PieceType, Square}
+import com.ffb.zugzwang.chess.{CastleRights, CastleSide, Color, GameState, MutablePosition, Piece, PieceType, Square}
 import com.ffb.zugzwang.rules.Rules
 
 object MoveGenerator:
@@ -10,6 +10,93 @@ object MoveGenerator:
       val newState = Rules.applyMove(state, move)
       !Rules.isSideInCheck(newState, state.activeSide)
     }
+
+  def pseudoLegalMovesMutable(position: MutablePosition): List[Move] =
+    val moves = MoveList(256)
+
+    val occupied     = position.occupied
+    val activePieces = position.byColor(position.activeSide.ordinal)
+    val targets      = position.byColor(position.activeSide.enemy.ordinal)
+    // this is the bulk of the moves. This should cover all of the attacks for every piece,
+    // and then quiet moves for all pieces except for pawns
+    Piece.byColor(position.activeSide) foreach { piece =>
+      // handle pawns separately
+      var pieces = position.pieces(piece)
+
+      while pieces.nonEmpty do
+        val from = Square(pieces.trailingZeros)
+        pieces = pieces.removeLsb
+
+        val rawAttacks = Attacks.attacks(piece, from, occupied)
+        var attackMask = rawAttacks & targets
+        var quietMask  = rawAttacks & ~occupied
+
+        if piece == Piece.WhitePawn || piece == Piece.BlackPawn then
+          var pawnAttacks = rawAttacks & (attackMask | (position.enPassantSq match
+            case Some(sq) => 1L << sq.value
+            case None     => 0L
+          ))
+          // first generate pawn attacks
+          while pawnAttacks.nonEmpty do
+            val to = Square(pawnAttacks.trailingZeros)
+            pawnAttacks = pawnAttacks.removeLsb
+
+            if position.enPassantSq.isDefined && to == position.enPassantSq.get then
+              position.enPassantSq.foreach(sq => moves.add(Move(from, to, MoveType.EnPassant)))
+            else if to.lastRank(position.activeSide) then addPromotions(from, to, isCapture = true, moves)
+            else moves.add(Move(from, to, MoveType.Capture))
+
+          // TODO: can i make this faster by breaking this out and doing all the pawns in one shot?
+          // now handle pawn pushes
+          var singlePush =
+            ~occupied & (position.activeSide match
+              case Color.White => (1L << from.value) << 8
+              case Color.Black => (1L << from.value) >>> 8
+            )
+          var doublePush =
+            ~occupied & (position.activeSide match
+              case Color.White => singlePush << 8
+              case Color.Black => singlePush >>> 8
+            ) // (singlePush << direction)
+
+          while singlePush.nonEmpty do
+            val to = Square(singlePush.trailingZeros)
+            singlePush = singlePush.removeLsb
+
+            if to.lastRank(position.activeSide) then addPromotions(from, to, isCapture = false, moves)
+            else moves.add(Move(from, to, MoveType.Quiet))
+
+          if from.startingPawnRank(position.activeSide) then
+            while doublePush.nonEmpty do
+              val to = Square(doublePush.trailingZeros)
+              doublePush = doublePush.removeLsb
+
+              moves.add(Move(from, to, MoveType.DoublePush))
+        else
+          while attackMask.nonEmpty do
+            val to = Square(attackMask.trailingZeros)
+            attackMask = attackMask.removeLsb
+
+            moves.add(Move(from, to, MoveType.Capture))
+
+          while quietMask.nonEmpty do
+            val to = Square(quietMask.trailingZeros)
+            quietMask = quietMask.removeLsb
+
+            moves.add(Move(from, to, MoveType.Quiet))
+    }
+
+    // castles
+    if !position.castleRights.isEmpty then
+      castleMovesMutable(
+        occupied,
+        position.activeSide,
+        position.castleRights,
+        position,
+        moves
+      )
+
+    moves.toList
 
   def pseudoLegalMoves(state: GameState): List[Move] =
     val moves = MoveList(256)
@@ -98,6 +185,18 @@ object MoveGenerator:
 
     moves.toList
 
+  private def castleMovesMutable(
+    occupied: Bitboard,
+    activeSide: Color,
+    castleRights: CastleRights,
+    position: MutablePosition,
+    moves: MoveList
+  ): Unit =
+    if castleRights.has(activeSide, CastleSide.Kingside)
+    then kingsideCastlesMutable(occupied, activeSide, position, moves)
+    if castleRights.has(activeSide, CastleSide.Queenside)
+    then queensideCastlesMutable(occupied, activeSide, position, moves)
+
   private def castleMoves(
     occupied: Bitboard,
     activeSide: Color,
@@ -109,6 +208,24 @@ object MoveGenerator:
     then kingsideCastles(occupied, activeSide, board, moves)
     if castleRights.has(activeSide, CastleSide.Queenside)
     then queensideCastles(occupied, activeSide, board, moves)
+
+  private def kingsideCastlesMutable(
+    occupied: Bitboard,
+    activeSide: Color,
+    position: MutablePosition,
+    moves: MoveList
+  ): Unit =
+    val (squares, mask) =
+      if activeSide == Color.White then (List(Square.F1, Square.G1), Bitboard.f1g1Mask)
+      else (List(Square.F8, Square.G8), Bitboard.f8g8mask)
+
+    val (from, to) =
+      if activeSide == Color.White then (Square.E1, Square.G1)
+      else (Square.E8, Square.G8)
+
+    if squares.forall(!position.isSquareAttacked(_, activeSide.enemy))
+      && (occupied & mask).isEmpty && !position.isKingAttacked
+    then moves.add(Move(from, to, MoveType.CastleKingside))
 
   private def kingsideCastles(
     occupied: Bitboard,
@@ -128,6 +245,23 @@ object MoveGenerator:
         !board.isAttacked(_, activeSide)
       ) && (occupied & mask).isEmpty && !board.isKingAttacked(activeSide)
     then moves.add(Move(from, to, MoveType.CastleKingside))
+
+  private def queensideCastlesMutable(
+    occupied: Bitboard,
+    activeSide: Color,
+    position: MutablePosition,
+    moves: MoveList
+  ): Unit =
+    val (squares, mask) =
+      if activeSide == Color.White then (List(Square.C1, Square.D1), Bitboard.b1c1d1Mask)
+      else (List(Square.C8, Square.D8), Bitboard.b8c8d8Mask)
+
+    val (from, to) =
+      if activeSide == Color.White then (Square.E1, Square.C1)
+      else (Square.E8, Square.C8)
+
+    if squares.forall(!position.isSquareAttacked(_, activeSide.enemy)) && (occupied & mask).isEmpty && !position.isKingAttacked
+    then moves.add(Move(from, to, MoveType.CastleQueenside))
 
   private def queensideCastles(
     occupied: Bitboard,
