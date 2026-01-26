72621029fb09dd50f581ae6e2f32038cb1ef26bc
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2026-01-26 11:44:25 -0500

    Add simple piece tables for other pieces

--- engine/src/main/scala/com/ffb/zugzwang/evaluation/PieceSquareTables.scala
+++ engine/src/main/scala/com/ffb/zugzwang/evaluation/PieceSquareTables.scala
@@ -25,12 +25,59 @@ object PieceSquareTables:
     -50, -40, -30, -30, -30, -30, -40, -50
   )
 
+  private val BishopTable = Array(
+    -20, -10, -10, -10, -10, -10, -10, -20, // Rank 1
+    -10, 5, 0, 0, 0, 0, 5, -10,             // Rank 2
+    -10, 10, 10, 10, 10, 10, 10, -10,       // Rank 3
+    -10, 0, 10, 10, 10, 10, 0, -10,         // Rank 4
+    -10, 5, 5, 10, 10, 5, 5, -10,           // Rank 5
+    -10, 0, 5, 10, 10, 5, 0, -10,           // Rank 6
+    -10, 0, 0, 0, 0, 0, 0, -10,             // Rank 7
+    -20, -10, -10, -10, -10, -10, -10, -20  // Rank 8
+  )
+
+  private val RookTable = Array(
+    0, 0, 0, 5, 5, 0, 0, 0,           // Rank 1
+    -5, 0, 0, 0, 0, 0, 0, -5,         // Rank 2
+    -5, 0, 0, 0, 0, 0, 0, -5,         // Rank 3
+    -5, 0, 0, 0, 0, 0, 0, -5,         // Rank 4
+    -5, 0, 0, 0, 0, 0, 0, -5,         // Rank 5
+    -5, 0, 0, 0, 0, 0, 0, -5,         // Rank 6
+    5, 10, 10, 10, 10, 10, 10, 10, 5, // Rank 7
+    0, 0, 0, 0, 0, 0, 0, 0            // Rank 8
+  )
+
+  private val QueenTable = Array(
+    -20, -10, -10, -5, -5, -10, -10, -20, // Rank 1
+    -10, 0, 0, 0, 0, 5, 0, -10,           // Rank 2
+    -10, 0, 5, 5, 5, 5, 5, -10,           // Rank 3
+    -5, 0, 5, 5, 5, 5, 0, 0,              // Rank 4
+    -5, 0, 5, 5, 5, 5, 0, -5,             // Rank 5
+    -10, 0, 5, 5, 5, 5, 0, -10,           // Rank 6
+    -10, 0, 0, 0, 0, 0, 0, -10,           // Rank 7
+    -20, -10, -10, -5, -5, -10, -10, -20  // Rank 8
+  )
+
+  private val KingTable = Array(
+    20, 30, 10, 0, 0, 10, 30, 20,           // Rank 1
+    20, 20, 0, 0, 0, 0, 20, 20,             // Rank 2
+    -10, -20, -20, -20, -20, -20, -20, -10, // Rank 3
+    -20, -30, -30, -40, -40, -30, -30, -20, // Rank 4
+    -30, -40, -40, -50, -50, -40, -40, -30, // Rank 5
+    -30, -40, -40, -50, -50, -40, -40, -30, // Rank 6
+    -30, -40, -40, -50, -50, -40, -40, -30, // Rank 7
+    -30, -40, -40, -50, -50, -40, -40, -30  // Rank 8
+  )
+
   def value(pt: PieceType, sq: Square, color: Color): Int =
     val table = pt match
       case PieceType.Pawn   => PawnTable
       case PieceType.Knight => KnightTable
-      // TODO: add other piece tables here
-      case _ => return 0
+      case PieceType.Bishop => BishopTable
+      case PieceType.Rook   => RookTable
+      case PieceType.Queen  => QueenTable
+      case PieceType.King   => KingTable
+      case _                => return 0
 
     // Rank mirroring: square ^ 56  (XOR 56 flips the rank index 0->7, 1->6)
     val index = if color == Color.White then sq.value else (sq.value ^ 56)
