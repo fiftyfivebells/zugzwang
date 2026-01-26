31e7dc75ecfda9ecba4b219acdc053d9df493454
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2026-01-26 09:30:31 -0500

    Reverse pawn table to match square ordering

--- engine/src/main/scala/com/ffb/zugzwang/evaluation/PieceSquareTables.scala
+++ engine/src/main/scala/com/ffb/zugzwang/evaluation/PieceSquareTables.scala
@@ -4,14 +4,14 @@ import com.ffb.zugzwang.chess.{Color, PieceType, Square}
 
 object PieceSquareTables:
   private val PawnTable = Array(
-    0, 0, 0, 0, 0, 0, 0, 0,         // Rank 8 (Promoted, shouldn't exist)
-    50, 50, 50, 50, 50, 50, 50, 50, // Rank 7 (About to promote)
-    10, 10, 20, 30, 30, 20, 10, 10, // Rank 6
-    5, 5, 10, 25, 25, 10, 5, 5,     // Rank 5
-    0, 0, 0, 20, 20, 0, 0, 0,       // Rank 4 (Center Control!)
-    5, -5, -10, 0, 0, -10, -5, 5,   // Rank 3
+    0, 0, 0, 0, 0, 0, 0, 0,         // Rank 1
     5, 10, 10, -20, -20, 10, 10, 5, // Rank 2
-    0, 0, 0, 0, 0, 0, 0, 0          // Rank 1
+    5, -5, -10, 0, 0, -10, -5, 5,   // Rank 3
+    0, 0, 0, 20, 20, 0, 0, 0,       // Rank 4 (Center Control!)
+    5, 5, 10, 25, 25, 10, 5, 5,     // Rank 5
+    10, 10, 20, 30, 30, 20, 10, 10, // Rank 6
+    50, 50, 50, 50, 50, 50, 50, 50, // Rank 7 (About to promote)
+    0, 0, 0, 0, 0, 0, 0, 0          // Rank 8 (Promoted, shouldn't exist)
   )
 
   private val KnightTable = Array(
@@ -22,7 +22,7 @@ object PieceSquareTables:
     -30, 5, 15, 20, 20, 15, 5, -30,         // Center is good (rank 5)
     -30, 0, 10, 15, 15, 10, 0, -30,         // Rank 6
     -40, -20, 0, 0, 0, 0, -20, -40,         // Rank 7
-    -50, -40, -30, -30, -30, -30, -40, -50  // Rank 8
+    -50, -40, -30, -30, -30, -30, -40, -50
   )
 
   def value(pt: PieceType, sq: Square, color: Color): Int =
